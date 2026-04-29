24b77afe0053c8d8ea19ad38b9b28b9919918e7d
Ntambwe <tataliaskitambwe@gmail.com>
2026-04-29 20:42:57 +0200

    Mon premier envoi

---
+++ index.js
@@ -0,0 +1,9 @@
+const colors = require('colors');
+
+console.log('===================================='.rainbow);
+console.log('   BIENVENUE DANS LE PROJET DE'.bold);
+console.log('             LEONIDAS'.cyan.underline);
+console.log('===================================='.rainbow);
+
+console.log('\n[Statut] : '.grey + 'Environnement configure'.green);
+console.log('[Info]   : '.grey + 'Node.js et NPM fonctionnent'.yellow);
