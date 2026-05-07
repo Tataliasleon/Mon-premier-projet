5cf30a2eb4c1005257ef19552b89facef07a471e
Ntambwe <tataliaskitambwe@gmail.com>
2026-05-07 20:06:11 +0200

    Correction du lien de base de donnees

--- M3-Pro-Platform/index.js
+++ M3-Pro-Platform/index.js
@@ -3,4 +3,13 @@ const Entreprise = mongoose.model('Entreprise', {
     secteur: String,
     chiffre: Number,
     cote: { type: Number, default: 1.5 }
-});
+});const dbURI = process.env.MONGO_URI;
+
+mongoose.connect(dbURI)
+  .then(() => {
+    console.log("Connecté à MongoDB !");
+    app.listen(port, () => {
+      console.log(`Serveur lancé sur le port ${port}`);
+    });
+  })
+  .catch((err) => console.log("Erreur de connexion :", err));
