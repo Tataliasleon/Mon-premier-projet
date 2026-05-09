fdec10aa03e0ba65d0f9e18ee3c43ad118dfa57d
Ntambwe <tataliaskitambwe@gmail.com>
2026-05-09 18:20:39 +0200

    Force HTML

--- M3-Pro-Platform/index.js
+++ M3-Pro-Platform/index.js
@@ -1,13 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const app = express();
+const port = process.env.PORT || 10000;
 
-// 1. CONFIGURATION DU MOTEUR DE RENDU (Si tu utilises EJS)
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// 2. PRIORITÉ : Servir les fichiers du dossier public
+// PRIORITÉ N°1 : Servir le dossier public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 3. FORCE L'AFFICHAGE DE TON INDEX.HTML SUR LA PAGE D'ACCUEIL
+// PRIORITÉ N°2 : Forcer l'envoi du fichier HTML sur la racine
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+// CONNEXION BDD
+const dbURI = process.env.MONGO_URI;
+mongoose.connect(dbURI)
+  .then(() => {
+    console.log("✅ Connexion MongoDB OK");
+    app.listen(port, () => console.log(`🚀 Serveur sur port ${port}`));
+  })
+  .catch(err => console.log(err));
