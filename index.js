372e3c8615978444bc575e31520d02f5591c2c12
Ntambwe <tataliaskitambwe@gmail.com>
2026-05-10 13:50:51 +0200

    Structure finale corrigee et propre

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 10000;
 
-// On lie le dossier "public" pour le design
+// On lie le dossier "public" pour ton design
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Connexion à ta base de données MongoDB
+// Connexion MongoDB
 const dbURI = process.env.MONGO_URI;
 mongoose.connect(dbURI)
   .then(() => {
-    console.log("✅ Connexion MongoDB OK");
+    console.log("✅ MongoDB Connecté");
     app.listen(port, () => console.log(`🚀 Serveur actif sur le port ${port}`));
   })
   .catch(err => console.log("❌ Erreur :", err));
 
-// Route pour afficher ton site
+// Route pour afficher ton design
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
